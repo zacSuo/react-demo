3bffde976f04b6848d2a0ab04dbb15d7a06ca319
zac <suoxd123@126.com>
2019-01-23 12:25:57 +0800

    下棋

--- src/index.js
+++ src/index.js
@@ -2,23 +2,47 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Square extends React.Component {
-  render() {
+function Square(props){
     return (
-      <button className="square">
-        {/* TODO */}
+      <button className="square" onClick={props.onClick}>
+        {props.value}
       </button>
     );
-  }
 }
 
 class Board extends React.Component {
+
+  constructor(){
+    super();
+    this.state = {
+        squares:Array(9).fill(null),
+        xIsNext:true,
+    }
+  }
   renderSquare(i) {
-    return <Square />;
+    return (
+        <Square 
+            value={this.state.squares[i]}
+            onClick = {() => this.handleClick(i)}
+        />
+    );
+  }
+
+  getStrByNext(){
+    return this.state.xIsNext?'X':'O';
+  }
+
+  handleClick(i){
+    const squares = this.state.squares.slice();
+    squares[i] = this.getStrByNext();
+    this.setState({
+        squares:squares,
+        xIsNext: !this.state.xIsNext,
+    })
   }
 
   render() {
-    const status = 'Next player: X';
+    const status = 'Next player: ' + this.getStrByNext();
 
     return (
       <div>
