7763db4abce3dde1961876073f04b818e17d6d6d
zac <suoxd123@126.com>
2019-01-23 13:22:39 +0800

    结束规则

--- src/index.js
+++ src/index.js
@@ -10,6 +10,27 @@ function Square(props){
     );
 }
 
+function calculateWinner(squares){
+    const lines = [    
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+    ];
+    for (let i=0;i<lines.length;i++){
+        const [a,b,c] = lines[i];
+        if(squares[a] && squares[a] == squares[b] && squares[a] == squares[c])
+        {
+            return squares[a];
+        }
+    }
+    return null;
+}
+
 class Board extends React.Component {
 
   constructor(){
@@ -34,6 +55,9 @@ class Board extends React.Component {
 
   handleClick(i){
     const squares = this.state.squares.slice();
+    if(calculateWinner(squares) || squares[i]){
+        return;
+    }
     squares[i] = this.getStrByNext();
     this.setState({
         squares:squares,
@@ -42,7 +66,13 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = 'Next player: ' + this.getStrByNext();
+    const winner = calculateWinner(this.state.squares);
+    let status;
+    if(winner){
+        status = 'Winner: ' + winner;
+    }else{
+        status = 'Next player: ' + this.getStrByNext();
+    }
 
     return (
       <div>
